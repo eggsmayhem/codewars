c793acd64bf3d595d338471bc69f457cdf3eedcb
eggsmayhem <flipmybit@protonmail.com>
2022-02-08 11:46:42 -0500

    104

---
+++ js/solution104.js
@@ -0,0 +1,16 @@
+function solution(number){
+  let answer = []
+  let numbers = []
+  for (let i = 0; i < number; i ++) {
+    numbers.push(i)
+  }
+  
+  for (let j = 0; j < numbers.length; j++) {
+    if (numbers[j] % 3 === 0 || numbers[j] % 5 === 0) {
+      answer.push(numbers[j])
+    }
+  }
+  
+  return answer.reduce((sum,x)=> sum + x, 0)
+  
+}
