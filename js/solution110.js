e65d03499e216630ed65bd6a2524855cbf753fdc
eggsmayhem <flipmybit@protonmail.com>
2022-02-14 11:48:02 -0500

    110

---
+++ js/solution110.js
@@ -0,0 +1,29 @@
+function sumDigPow(a, b) {
+  let arr = []
+  let counter = a;
+  while (counter<=b) {
+    arr.push(counter)
+    counter++
+  }
+  
+  console.log(arr)
+  
+  let answer = []
+  
+  
+  for (let i =0; i<arr.length; i++){
+    let sum = 0
+    let inc = 1
+    let digits = arr[i].toString().split('').map(x=>parseInt(x))
+    for (let j = 0; j < digits.length; j++){
+      sum+=(digits[j])**inc
+      inc++
+    }
+    if (sum===arr[i]) {
+      answer.push(arr[i])
+    }
+  }
+  
+  return answer
+  
+}
