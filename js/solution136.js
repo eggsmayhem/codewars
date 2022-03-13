f7020634148732a56aeb97252e40158badef5bff
eggsmayhem <flipmybit@protonmail.com>
2022-03-13 12:55:28 -0400

    136

---
+++ js/solution136.js
@@ -0,0 +1,15 @@
+function isPangram(string){
+  let letters = []
+  let arr = string.toLowerCase().split('')
+  for (let i = 0; i <arr.length; i++) {
+    if (!letters.includes(arr[i]) && arr[i].match(/[a-z]/gi)) {
+      letters.push(arr[i])
+    }
+  }
+  console.log(arr)
+  console.log(letters)
+  if (letters.length === 26) {
+    return true
+  }
+  return false
+}
