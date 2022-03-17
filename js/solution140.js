6222b6348c054d5ad08728003247fc4272cf472c
eggsmayhem <flipmybit@protonmail.com>
2022-03-17 12:56:26 -0400

    140

---
+++ js/solution140.js
@@ -0,0 +1,9 @@
+function queueTime(customers, n) {
+  let lines = new Array(n).fill(0)
+  for (let person of customers) {
+    let index = lines.indexOf(Math.min(...lines))
+    lines[index]+=person
+  }
+  return Math.max(...lines)
+ 
+}
