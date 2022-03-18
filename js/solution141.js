0cd40827d57a879a90779022f7738c787c739ad4
eggsmayhem <flipmybit@protonmail.com>
2022-03-18 12:56:52 -0400

    141

---
+++ js/solution141.js
@@ -0,0 +1,12 @@
+function findEvenIndex(arr){
+  let leftTotal = 0
+  let rightTotal = 0
+  for (let i = 0; i < arr.length; i++) {
+    leftTotal = arr.slice(0,i).reduce((sum,x) => sum + x, 0)
+    rightTotal = arr.slice(i+1).reduce((sum,x) => sum + x, 0)
+    if (leftTotal === rightTotal) {
+      return i
+    }
+  }
+  return -1
+}
