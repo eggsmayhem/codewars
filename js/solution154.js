09dc2c824d19f20236c80ada474064080b5b7439
eggsmayhem <flipmybit@protonmail.com>
2022-04-01 13:01:14 -0400

    154

---
+++ js/solution154.js
@@ -0,0 +1,7 @@
+function isNarcissistic(n) {
+//   let testNum = n
+  let l = n.toString().length
+  let arr = n.toString().split('')
+  return n === arr.reduce((narc, item) => narc + (item ** l), 0)
+
+}
