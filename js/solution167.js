2894899f0811392efb635d420e06808d473ae967
eggsmayhem <flipmybit@protonmail.com>
2022-04-13 13:04:54 -0400

    167

---
+++ js/solution167.js
@@ -0,0 +1,19 @@
+function likes(names) {
+  const count = names.length;
+  if (count === 0) {
+      return 'no one likes this'
+  }
+  if (count === 1) {
+    return `${names[0]} likes this`
+  }
+  if (count === 2) {
+    return `${names[0]} and ${names[1]} like this`
+  }
+  if (count === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  }
+  else {
+    return `${names[0]}, ${names[1]} and ${count-2} others like this`
+  }
+  
+}
