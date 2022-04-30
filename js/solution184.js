570b49bfa69b9dc3d6fcb64542fc42a12339e5cf
eggsmayhem <flipmybit@protonmail.com>
2022-04-30 13:08:37 -0400

    184

---
+++ js/solution184.js
@@ -0,0 +1,11 @@
+function dontGiveMeFive(start, end) {
+  let arr = [];
+  for (let i = start; i <= end; i++) {
+    if (!i.toString().includes('5')) {
+      arr.push(i)
+    }
+  }
+  
+  return arr.length;
+    
+}
