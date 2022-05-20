f0b1e9aec0f8c78795f0ac52352cd3b7abfde443
eggsmayhem <flipmybit@protonmail.com>
2022-05-19 21:45:49 -0400

    solution009 Barista problem

---
+++ js/solution009.js
@@ -0,0 +1,14 @@
+//Barista problem 7kyu
+//
+function barista(coffees){
+  let sorted = coffees.sort((a,b) => a - b );
+  
+  let wait = 0;
+  while(sorted.length) {
+    wait += sorted[0] * sorted.length;
+    sorted.shift()
+    wait += sorted.length * 2
+  }
+  
+  return wait;
+}
