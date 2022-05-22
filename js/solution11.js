b8184a74a50bd71003102763c14ef931e25f407e
eggsmayhem <flipmybit@protonmail.com>
2022-05-22 19:38:12 -0400

    calculating interest

---
+++ js/solution11.js
@@ -0,0 +1,18 @@
+//given P r and n, return [simple interest, compound interest]
+//
+function interest(P,r,n) {
+  console.log(`P : ${P}`)
+  console.log(`r: ${r}`)
+  console.log(`n: ${n}`)
+  let simple = Math.round(P + (P * r) * n)
+  let amount;
+
+  for (let i=1; i<=n; i++) {
+    amount = Math.round(P * Math.pow( 1.0 + r, i ));
+  }
+  
+  if (n===0) {
+    amount = P
+  }
+  return [simple, amount]
+}
