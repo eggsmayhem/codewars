1f6a26ef3ccd08006b16976fd5edf6ca8ba05cb2
eggsmayhem <flipmybit@protonmail.com>
2022-06-30 17:38:27 -0400

    Find the missing term in an Arithmetic Progression

---
+++ js/solution42.js
@@ -0,0 +1,8 @@
+Find the missing term in an Arithmetic Progression
+
+var findMissing = function (list) {
+  const space = (list[list.length-1] - list[0]) / list.length
+
+  return list.filter((item,index)=>item !== list[0] + index * space)[0] - space
+
+}
