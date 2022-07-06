c8e025b767c249ca232e166bae5e72c355af67e7
eggsmayhem <flipmybit@protonmail.com>
2022-07-06 17:38:42 -0400

    Lario and Muigi Pipe Problem

---
+++ js/solution44.js
@@ -0,0 +1,15 @@
+Lario and Muigi Pipe Problem
+
+
+function pipeFix(numbers){
+  if (numbers.length === 1) {
+    return numbers
+  }
+  const first = numbers.shift()
+  const last = numbers.pop()
+  let answer = []
+  for (let i = first; i <= last; i++) {
+    answer.push(i)
+  }
+  return answer
+}
