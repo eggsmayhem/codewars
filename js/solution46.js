a75288c78d9e942670b1af7fe5eff42c63eaceba
eggsmayhem <flipmybit@protonmail.com>
2022-07-10 17:23:17 -0400

    Growth of a population

---
+++ js/solution46.js
@@ -0,0 +1,10 @@
+//Growth of a population
+//
+function nbYear(p0, percent, aug, p) {
+    let years = 0
+    while (p0 < p) {
+      p0 = Math.floor(p0 + (p0 * (percent * .01)) + aug)
+      years++
+    }
+    return years
+}
