37cccc94616096fb2be84700f56425d7929e0b7d
eggsmayhem <flipmybit@protonmail.com>
2022-07-17 13:58:03 -0400

    Highest and lowest

---
+++ js/solution48.js
@@ -0,0 +1,9 @@
+//Highest and lowest
+//
+function highAndLow(numbers){
+  const nums = numbers.split(' ').map(x => parseInt(x))
+  const arr = []
+  arr.push(Math.max(...nums))
+  arr.push(Math.min(...nums))
+  return arr.join(' ')
+}
