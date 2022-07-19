7d5edd8c78a3d6f8328e365a0f2624a32d993562
eggsmayhem <flipmybit@protonmail.com>
2022-07-18 21:09:07 -0400

    Complementary DNA

---
+++ js/solution49.js
@@ -0,0 +1,12 @@
+//Complementary DNA
+//
+function DNAStrand(dna){
+  const pairs = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C",
+  }
+  
+  return dna.split('').map(x=>pairs[x]).join('')
+}
