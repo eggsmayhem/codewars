adb0b67be378300e3f981be3363141f1db25e445
eggsmayhem <flipmybit@protonmail.com>
2022-07-24 21:45:13 -0400

    Vowel Count

---
+++ js/solution51.js
@@ -0,0 +1,14 @@
+//Vowel Count
+//
+function getCount(str) {
+  let count = 0
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  for (let i = 0; i< str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++
+    }
+  }
+  return count 
+  
+  
+}
