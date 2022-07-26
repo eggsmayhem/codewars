0e618cf1997f032d4b98bc4976435625490c4fbb
eggsmayhem <flipmybit@protonmail.com>
2022-07-25 20:21:21 -0400

    refactored solution 53 from for loop to forEach

--- js/solution53.js
+++ js/solution53.js
@@ -1,13 +1,28 @@
 //LC Running Sum of 1d array
 //
+
 const runningSum = function(nums) {
  
     const answer = new Array(nums.length).fill(0)
     
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j <=i; j++) {
-            answer[i]+=nums[j]
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = 0; j <=i; j++) {
+    //         answer[i]+=nums[j]
+    //     }
+    // }
+    nums.forEach((item, index) => {
+        for (let i = 0; i<=index; i++) {
+            answer[index]+=nums[i]
         }
-    }
+    })
     return answer
-};
+}; 
+
+
+
+
+
+
+
+
+
