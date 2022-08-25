4ceeaf84211e795c7f9227f3f1ef4a620f420fab
eggsmayhem <flipmybit@protonmail.com>
2022-08-25 13:00:52 -0400

    Capitalize first letter of a string

---
+++ js/solution71.js
@@ -0,0 +1,9 @@
+//Capitalize first letter of a String
+
+
+String.prototype.capitalize = function () {
+  const capital = {a:"A",b:"B",c:"C",d:"D",e:"E",f:"F",g:"G",h:"H",i:"I",j:"J",k:"K",l:"L",m:"M",n:"N",o:"O",p:"P",q:"Q",r:"R",s:"S",t:"T",u:"U",v:"V",w:"W",x:"X",y:"Y",z:"Z"}
+  return capital[this[0]] ? capital[this[0]] + this.slice(1) : this.toString()
+}
+
+
