da3dafc7c378ad8c4c755c7de1e29c56355d5939
Sabrina Pyles <flipmybit@protonmail.com>
2022-11-05 12:03:23 -0400

    314

---
+++ js/solution314.js
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+int grow(size_t size, const int arr[size]) {
+  int prod = 1;
+  for (int i = 1; i <= size; i++) {
+    prod = prod * arr[i - 1];
+  }
+  return prod; //Your code comes here
+#include <stddef.h>
+
+int grow(size_t size, const int arr[size]) {
+  int prod = 1;
+  for (int i = 1; i <= size; i++) {
+    prod = prod * arr[i - 1];
+  }
+  return prod; //Your code comes here
+}
+~
+~
+~
+}
