56d0a39d448ed4e5f291682e5365d10588d8e1cf
Sabrina Pyles <flipmybit@protonmail.com>
2022-11-06 09:34:56 -0500

    289

---
+++ js/solution289.js
@@ -0,0 +1,18 @@
+//LC 121
+//
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let low = prices[0];
+    let profit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        // let temp = prices[i] - buyLow;
+        // if (prices[i] < buyLow) buyLow = prices[i];
+        // maxProfit = Math.max(maxProfit, temp);
+        low = Math.min(low, prices[i]);
+        profit = Math.max(profit, prices[i] - low);
+    }
+    return profit;
+};
