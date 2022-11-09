379892bcc8abdfbe94ca20eb3d6e773197914361
hunter <flipmybit@protonmail.com>
2022-11-09 00:02:08 +0000

    solution271.js

---
+++ js/solution271.js
@@ -0,0 +1,7 @@
+//Sum of Digitals Roots (recursive!)
+//
+function digitalRoot(n) {
+  if (n.toString().length === 1) return n;
+  let num = n.toString().split('').map(x=>Number(x)).reduce((sum, item) => sum + item, 0);
+  return digitalRoot(num)
+}
