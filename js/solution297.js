b954a192146ba9adfb539c8cc593d438de8a4ecb
Sabrina Pyles <flipmybit@protonmail.com>
2022-12-11 09:40:24 -0500

    297

---
+++ js/solution297.js
@@ -0,0 +1,51 @@
+// LC 232
+//
+
+var MyQueue = function() {
+    this.stack = [];
+    this.helperStack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    while (this.stack.length) {
+        this.helperStack.push(this.stack.pop())
+    }
+    this.stack.push(x);
+    while (this.helperStack.length) {
+        this.stack.push(this.helperStack.pop())
+    }
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack[this.stack.length -1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack.length === 0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
